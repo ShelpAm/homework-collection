be4eb35d8aaf5c7d199ac5686d03d03d6b22acd9
ShelpAm <shelpam@qq.com>
2024-09-25 22:10:48 +0800

    chore: update current uploading/receiving assignment

--- src/load-data.go
+++ src/load-data.go
@@ -38,6 +38,7 @@ func LoadAssignments(assignments *map[string]Assignment) error {
 	(*assignments)["第二周"] = Assignment{"第二周", time.Now(), time.Now().Add(time.Hour * 24 * 7)}
 	// (*assignments)["第三周"] = Assignment{"第三周", time.Now(), time.Date(2024, 9, 21, 18, 0, 0, 100, time.Local)}
 	(*assignments)["第三周"] = Assignment{"第三周", time.Now(), time.Now().Add(time.Hour * 24 * 7)}
+	(*assignments)["第四周"] = Assignment{"第四周", time.Now(), time.Now().Add(time.Hour * 24 * 7)}
 
 	return nil
 }
